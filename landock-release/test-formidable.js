497624ce151a281e70bf102e63ed2f182ec20de5
Aanish Farrukh <249387242+sudoaanish@users.noreply.github.com>
2026-07-07 05:59:10 -0400

    Remove test-formidable.js script

--- landock-release/test-formidable.js
+++
@@ -1,4 +0,0 @@
-const formidable = require('formidable');
-console.log('Formidable exports type:', typeof formidable);
-console.log('Formidable keys:', Object.keys(formidable));
-console.log('Formidable structure:', formidable);
